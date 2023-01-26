13f9685bc252e925b643d4da4d79394b6da59297
maspaitujaki <13520156@std.stei.itb.ac.id>
2023-01-26 16:03:36 +0700

    feat: add affine cipher algorithm

--- Cipher-Algorithm/affine_cipher.py
+++ Cipher-Algorithm/affine_cipher.py
@@ -1,9 +1,53 @@
 import lib
 
-def affine_enc(p: str, b: int):
+relative_prime_26 = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+
+def affine_enc(p: str, b: int, m: int):
     # Pre Porcessing
 
     # Plain teks hanya alfabet tanpa angka, spasi, dan tanda baca
     p = lib.removePunctuation(p)
     p = lib.removeNumber(p)
     p = lib.removeSpace(p)
+
+    letters : list = list(p)
+
+    c = list(map(enc_char,letters,[b for _ in range(len(letters))],[m for _ in range(len(letters))]))
+    return "".join(c)
+
+def enc_char(p:str, b:int, m:int):
+    c = m * lib.alphabetToNumber(p) + b
+    # print(f"{m} * {lib.alphabetToNumber(p)} + {b} = {c}", end='')
+    c = c % 26
+    # print(f" = {c} (mod 26)")
+    return chr(c + 97)
+
+def affine_dec(c: str, b: int, m: int):
+
+    c = lib.removePunctuation(c)
+    c = lib.removeNumber(c)
+    c = lib.removeSpace(c)
+
+    letters : list = list(c)
+
+    p = list(map(dec_char,letters,[b for _ in range(len(letters))],[m for _ in range(len(letters))]))
+    
+    return "".join(p)
+
+
+def dec_char(c:str, b:int, m:int):
+    m_inverse = lib.modInverse(m,26)
+
+    p = m_inverse * (lib.alphabetToNumber(c) - b)
+    p = p % 26
+    return chr(p + 97)
+
+
+if __name__ == '__main__':
+    p = 'kripto'
+    b = 10
+    m = 7
+    c = affine_enc(p,b,m)
+    print(c)
+    p = affine_dec(c,b,m)
+    print(p)
--- Cipher-Algorithm/lib.py
+++ Cipher-Algorithm/lib.py
@@ -1,7 +1,7 @@
 import re
 
 def alphabetToNumber(c: str):
-    return ord(c.lower()) - 96
+    return ord(c.lower()) - 97
 
 def removeNumber(s: str):
     return ''.join([i for i in s if not i.isdigit()])
@@ -11,6 +11,13 @@ def removePunctuation(s: str):
 
 def removeSpace(s: str):
     return s.replace(" ", "")
+
+def modInverse(A: int, M: int):
+
+    for X in range(1, M):
+        if (((A % M) * (X % M)) % M == 1):
+            return X
+    return -1
 
 if __name__=='__main__':
     s = '1 2ab cd4.,"*05  '
