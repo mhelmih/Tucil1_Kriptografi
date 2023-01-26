27e5bb4ac444652727daa3ef0bf80f435462b14f
maspaitujaki <13520156@std.stei.itb.ac.id>
2023-01-26 19:06:19 +0700

    fix: Refactor affine_cipher.py

--- Cipher-Algorithm/affine_cipher.py
+++ Cipher-Algorithm/affine_cipher.py
@@ -2,52 +2,56 @@ import lib
 
 relative_prime_26 = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
 
-def affine_enc(p: str, b: int, m: int):
+def affine_encrypt(plaintext: str, pergeseran: int, relatifprima: int):
+    """ Enkripsi menggunakan affine cipher"""
     # Pre Porcessing
 
     # Plain teks hanya alfabet tanpa angka, spasi, dan tanda baca
-    p = lib.removePunctuation(p)
-    p = lib.removeNumber(p)
-    p = lib.removeSpace(p)
+    plaintext = lib.remove_punctuation(plaintext)
+    plaintext = lib.remove_number(plaintext)
+    plaintext = lib.remove_space(plaintext)
 
-    letters : list = list(p)
+    letters : list = list(plaintext)
 
-    c = list(map(enc_char,letters,[b for _ in range(len(letters))],[m for _ in range(len(letters))]))
-    return "".join(c)
+    cipher_text = list(map(encrypt_single_char,letters,[pergeseran for _ in range(len(letters))],[relatifprima for _ in range(len(letters))]))
+    return "".join(cipher_text).upper()
 
-def enc_char(p:str, b:int, m:int):
-    c = m * lib.alphabetToNumber(p) + b
+def encrypt_single_char(plainchar:str, pergeseran:int, relatifprima:int):
+    """ Enkripsi satu karakter dengan rumus"""
+    cipher_char = relatifprima * lib.alphabet_to_number(plainchar) + pergeseran
     # print(f"{m} * {lib.alphabetToNumber(p)} + {b} = {c}", end='')
-    c = c % 26
+    cipher_char = cipher_char % 26
     # print(f" = {c} (mod 26)")
-    return chr(c + 97)
+    return chr(cipher_char + 97)
 
-def affine_dec(c: str, b: int, m: int):
+def affine_decrypt(ciphertext: str, pergeseran: int, relatifprima: int):
+    """ Dekripsi string """
 
-    c = lib.removePunctuation(c)
-    c = lib.removeNumber(c)
-    c = lib.removeSpace(c)
+    ciphertext = lib.remove_punctuation(ciphertext)
+    ciphertext = lib.remove_number(ciphertext)
+    ciphertext = lib.remove_space(ciphertext)
 
-    letters : list = list(c)
+    letters : list = list(ciphertext)
 
-    p = list(map(dec_char,letters,[b for _ in range(len(letters))],[m for _ in range(len(letters))]))
+    plain_text = list(map(decrypt_char,letters,[pergeseran for _ in range(len(letters))],[relatifprima for _ in range(len(letters))]))
     
-    return "".join(p)
+    return "".join(plain_text).upper()
 
 
-def dec_char(c:str, b:int, m:int):
-    m_inverse = lib.modInverse(m,26)
+def decrypt_char(cipherchar:str, pergeseran:int, relatifprima:int):
+    """ dekripsi satu karakter menggunakan rumus """
+    m_inverse = lib.mod_inverse(relatifprima,26)
 
-    p = m_inverse * (lib.alphabetToNumber(c) - b)
-    p = p % 26
-    return chr(p + 97)
+    plain_char = m_inverse * (lib.alphabet_to_number(cipherchar) - pergeseran)
+    plain_char = plain_char % 26
+    return chr(plain_char + 97)
 
 
 if __name__ == '__main__':
     p = 'kripto'
     b = 10
     m = 7
-    c = affine_enc(p,b,m)
+    c = affine_encrypt(p,b,m)
     print(c)
-    p = affine_dec(c,b,m)
+    p = affine_decrypt(c,b,m)
     print(p)
