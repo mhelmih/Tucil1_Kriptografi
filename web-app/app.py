5e12cc6f4c5d6148fc762ef50bf8c229e265f872
maspaitujaki <13520156@std.stei.itb.ac.id>
2023-01-27 20:15:37 +0700

    feat: Add layout

--- web-app/app.py
+++ web-app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/affine_cipher/')
+def affine_cipher():
+    return render_template('affine_cipher.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
